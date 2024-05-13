6b908f921af83ff653de0489e8f6609e86ce87e3
Vitaliy Hula <gvnhak@gmail.com>
2024-05-13 15:20:42 +0300

    Merge branch 'dev' of github.com:Moonshot-Era/moonshot into supabase-init

--- frontend/src/app/layout.tsx
+++ frontend/src/app/layout.tsx
@@ -1,14 +1,34 @@
 import { GeistSans } from 'geist/font/sans';
+import { Inter } from 'next/font/google';
 import './globals.css';
+import { Metadata, Viewport } from 'next';
 
 const defaultUrl = process.env.VERCEL_URL
   ? `https://${process.env.VERCEL_URL}`
   : `${process.env.SITE_URL}`;
 
-export const metadata = {
+const inter = Inter({ subsets: ['latin'] });
+
+export const metadata: Metadata = {
   metadataBase: new URL(defaultUrl),
-  title: 'Next.js and Supabase Starter Kit',
-  description: 'The fastest way to build apps with Next.js and Supabase',
+  title: 'Moonshot',
+  description: 'Moonshot application',
+  generator: 'Next.js',
+  manifest: '/manifest.json',
+  keywords: ['nextjs', 'nextjs14', 'next14', 'pwa', 'next-pwa'],
+  authors: [{ name: 'Moonshot' }],
+  icons: [
+    { rel: 'apple-touch-icon', url: 'icons/icon-128x128.png' },
+    { rel: 'icon', url: 'icons/icon-128x128.png' },
+  ],
+};
+
+export const viewport: Viewport = {
+  themeColor: 'black',
+  viewportFit: 'cover',
+  minimumScale: 1,
+  initialScale: 1,
+  width: 'device-width',
 };
 
 export default function RootLayout({
