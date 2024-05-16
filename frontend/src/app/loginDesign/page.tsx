f531eb1b1f27a305b67913091049ce0799024e4c
vitalekp <vitalekpushkarov@gmail.com>
2024-05-16 10:06:19 +0300

    create Login layout

---
+++ frontend/src/app/loginDesign/page.tsx
@@ -0,0 +1,60 @@
+import Image from 'next/image';
+import { Flex, Text } from '@radix-ui/themes';
+
+import './style.scss';
+import { Button, Icon } from '@/legos';
+import cubistLogo from '../../assets/images/cubist_logo.svg';
+import { SplashScreen } from '@/components/SplashScreen/SplashScreen';
+
+export default function Login({
+  searchParams,
+}: {
+  searchParams: { message: string };
+}) {
+  return (
+    <>
+      <SplashScreen />
+      <Flex height="100vh" direction="column" align="center" justify="center">
+        <div className="login-figure1"></div>
+        <div className="login-figure2"></div>
+        <div className="login-figure3"></div>
+        <div className="login-figure4"></div>
+        <Flex
+          direction="column"
+          align="center"
+          justify="center"
+          flexGrow="1"
+          gap="8"
+        >
+          <Flex direction="column" align="center">
+            <Text size="8" weight="bold">
+              Moonshot
+            </Text>
+            <Text size="3" weight="medium">
+              Trade Culture
+            </Text>
+          </Flex>
+
+          <Flex direction="column" gap="4" width="100%">
+            <Button className="bg-white">
+              <Icon icon="google" width={24} />
+              <Text size="2" weight="medium">
+                Sign in with Google
+              </Text>
+            </Button>
+            <Button disabled className="bg-white">
+              <Icon icon="apple" width={24} />
+              <Text size="2" weight="medium">
+                Sign in with Apple
+              </Text>
+            </Button>
+          </Flex>
+        </Flex>
+        <Flex direction="row" position="absolute" bottom="50px">
+          <Text size="1">Powered by</Text>
+          <Image src={cubistLogo} alt="cubist-logo" />
+        </Flex>
+      </Flex>
+    </>
+  );
+}
