9e4be617423f6a7a87c5e1ab7d4833b72040f1df
vitalekp <vitalekpushkarov@gmail.com>
2024-05-17 17:31:33 +0300

    refactor

--- frontend/src/components/Header/Header.tsx
+++ frontend/src/components/Header/Header.tsx
@@ -5,39 +5,41 @@ import { Flex, Text } from '@radix-ui/themes';
 
 import './style.scss';
 import { Icon, NavButton } from '@/legos';
-import { usePathname } from 'next/navigation';
+import { usePathname, useRouter } from 'next/navigation';
 
 export const Header = () => {
   const pathname = usePathname();
+  const router = useRouter();
 
   return (
     <header className="bg-white">
       <nav>
         <Flex width="100%" justify="between" gap="2">
           <Link href="/">
-            <NavButton className={pathname === '/' ? 'nav-button-active' : ''}>
+            <NavButton
+              className={pathname === '/home-design' ? 'nav-button-active' : ''}
+              onClick={() => router.push('/home-design')}
+            >
               <Icon icon="home" width={16} />
               <Text size="1">Home</Text>
             </NavButton>
           </Link>
-          <Link href="/explore">
-            <NavButton
-              disabled
-              className={pathname === '/explore' ? 'nav-button-active' : ''}
-            >
-              <Icon icon="search" width={16} />
-              <Text size="1">Explore</Text>
-            </NavButton>
-          </Link>
-          <Link href="/settings">
-            <NavButton
-              disabled
-              className={pathname === '/settings' ? 'nav-button-active' : ''}
-            >
-              <Icon icon="settings" width={16} />
-              <Text size="1">Settings</Text>
-            </NavButton>
-          </Link>
+          <NavButton
+            disabled
+            className={pathname === '/explore' ? 'nav-button-active' : ''}
+            onClick={() => router.push('/explore')}
+          >
+            <Icon icon="search" width={16} />
+            <Text size="1">Explore</Text>
+          </NavButton>
+          <NavButton
+            disabled
+            className={pathname === '/settings' ? 'nav-button-active' : ''}
+            onClick={() => router.push('/settings')}
+          >
+            <Icon icon="settings" width={16} />
+            <Text size="1">Settings</Text>
+          </NavButton>
         </Flex>
       </nav>
     </header>
