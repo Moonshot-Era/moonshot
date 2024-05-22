f96a291a00f228a718006a17c7492c80b6a0599c
yuramol <yuramol86@gmail.com>
2024-05-22 12:23:04 +0300

    Use SplachScreen in layout

--- frontend/src/app/layout.tsx
+++ frontend/src/app/layout.tsx
@@ -6,6 +6,7 @@ import '@radix-ui/themes/styles.css';
 import './globals.scss';
 import './globals.css';
 import { Header } from '@/components/Header/Header';
+import { SplashScreen } from '@/components/SplashScreen/SplashScreen';
 
 const defaultUrl = process.env.VERCEL_URL
   ? `https://${process.env.VERCEL_URL}`
@@ -61,6 +62,7 @@ export default function RootLayout({
   return (
     <html lang="en">
       <body className={clashDisplayFont.className}>
+        <SplashScreen/>
         <Header />
         <Theme className="bg-transparent">{children}</Theme>
       </body>
