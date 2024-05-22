f760e42d8bc5d3372d4aebaaa093f0060cee067c
Anastasia <anastasia.pavlova.m@gmail.com>
2024-05-22 11:27:30 +0200

    feat: progressier notifications

--- frontend/src/app/layout.tsx
+++ frontend/src/app/layout.tsx
@@ -1,11 +1,12 @@
-import type { Metadata, Viewport } from 'next';
-import localFont from 'next/font/local';
-import { Theme } from '@radix-ui/themes';
-import '@radix-ui/themes/styles.css';
+import type { Metadata, Viewport } from "next";
+import localFont from "next/font/local";
+import { Theme } from "@radix-ui/themes";
+import "@radix-ui/themes/styles.css";
 
-import './globals.scss';
-import './globals.css';
-import { Header } from '@/components/Header/Header';
+import "./globals.scss";
+import "./globals.css";
+import { Header } from "@/components/Header/Header";
+import ServiceWorkerRegister from "@/components/ServiceWorkerRegister";
 
 const defaultUrl = process.env.VERCEL_URL
   ? `https://${process.env.VERCEL_URL}`
@@ -14,43 +15,43 @@ const defaultUrl = process.env.VERCEL_URL
 const clashDisplayFont = localFont({
   src: [
     {
-      path: '../assets/fonts/ClashDisplay-Regular.woff2',
-      weight: '400',
-      style: 'normal',
+      path: "../assets/fonts/ClashDisplay-Regular.woff2",
+      weight: "400",
+      style: "normal",
     },
     {
-      path: '../assets/fonts/ClashDisplay-Medium.woff2',
-      weight: '500',
-      style: 'normal',
+      path: "../assets/fonts/ClashDisplay-Medium.woff2",
+      weight: "500",
+      style: "normal",
     },
     {
-      path: '../assets/fonts/ClashDisplay-Semibold.woff2',
-      weight: '600',
-      style: 'normal',
+      path: "../assets/fonts/ClashDisplay-Semibold.woff2",
+      weight: "600",
+      style: "normal",
     },
   ],
 });
 
 export const metadata: Metadata = {
   metadataBase: new URL(defaultUrl),
-  title: 'Moonshot',
-  description: 'Moonshot application',
-  generator: 'Next.js',
-  manifest: '/manifest.json',
-  keywords: ['nextjs', 'nextjs14', 'next14', 'pwa', 'next-pwa'],
-  authors: [{ name: 'Moonshot' }],
+  title: "Moonshot",
+  description: "Moonshot application",
+  generator: "Next.js",
+  manifest: "/manifest.json",
+  keywords: ["nextjs", "nextjs14", "next14", "pwa", "next-pwa"],
+  authors: [{ name: "Moonshot" }],
   icons: [
-    { rel: 'apple-touch-icon', url: 'icons/icon-128x128.png' },
-    { rel: 'icon', url: 'icons/icon-128x128.png' },
+    { rel: "apple-touch-icon", url: "icons/icon-128x128.png" },
+    { rel: "icon", url: "icons/icon-128x128.png" },
   ],
 };
 
 export const viewport: Viewport = {
-  themeColor: 'black',
-  viewportFit: 'cover',
+  themeColor: "black",
+  viewportFit: "cover",
   minimumScale: 1,
   initialScale: 1,
-  width: 'device-width',
+  width: "device-width",
 };
 
 export default function RootLayout({
@@ -61,6 +62,15 @@ export default function RootLayout({
   return (
     <html lang="en">
       <body className={clashDisplayFont.className}>
+        <link
+          rel="manifest"
+          href={`https://progressier.app/${process.env.NEXT_PUBLIC_PROGRESSIER_ID}/progressier.json`}
+        />
+        <script
+          defer
+          src={`https://progressier.app/${process.env.NEXT_PUBLIC_PROGRESSIER_ID}/script.js`}
+        ></script>
+        <ServiceWorkerRegister />
         <Header />
         <Theme className="bg-transparent">{children}</Theme>
       </body>
