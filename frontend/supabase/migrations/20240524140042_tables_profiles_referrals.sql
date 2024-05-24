03e364da15a7a53cd13628b646b7c53f69b185bb
Vitaliy Hula <gvnhak@gmail.com>
2024-05-24 21:16:19 +0300

    Add referrals table. Implement trigger on add new user

--- frontend/supabase/migrations/20240524140042_tables_profiles_referrals.sql
+++ frontend/supabase/migrations/20240524140042_tables_profiles_referrals.sql
@@ -1,15 +1,15 @@
-
 create table "public"."referrals" (
     "cultureRef" text not null,
     "created_at" timestamp with time zone not null default now()
 );
 
-
 alter table "public"."profiles" add column "cultureRef" text;
 
-CREATE UNIQUE INDEX referals_pkey ON public.referrals USING btree ("cultureRef");
+alter table "public"."profiles" enable row level security;
 
-alter table "public"."referrals" add constraint "referals_pkey" PRIMARY KEY using index "referals_pkey";
+CREATE UNIQUE INDEX referrals_pkey ON public.referrals USING btree ("cultureRef");
+
+alter table "public"."referrals" add constraint "referrals_pkey" PRIMARY KEY using index "referrals_pkey";
 
 alter table "public"."profiles" add constraint "public_profiles_cultureRef_fkey" FOREIGN KEY ("cultureRef") REFERENCES referrals("cultureRef") ON UPDATE CASCADE ON DELETE RESTRICT not valid;
 
@@ -65,15 +65,23 @@ to public
 using ((( SELECT auth.uid() AS uid) = user_id));
 
 
-create policy "Enable insert for users based on user_id"
+create policy "Enable insert for authenticated users only"
 on "public"."profiles"
 as permissive
 for insert
+to authenticated
+with check (true);
+
+
+create policy "Enable read access for all users"
+on "public"."profiles"
+as permissive
+for select
 to public
-with check ((( SELECT auth.uid() AS uid) = user_id));
+using ((( SELECT auth.uid() AS uid) = user_id));
 
 
-create policy "Enable update for users based on email"
+create policy "Enable update for users based on userID"
 on "public"."profiles"
 as permissive
 for update
@@ -88,3 +96,17 @@ as permissive
 for select
 to public
 using (true);
+
+set check_function_bodies = off;
+
+CREATE OR REPLACE FUNCTION auth.create_new_profile()
+ RETURNS trigger
+ LANGUAGE plpgsql
+AS $function$begin
+  insert into public.profiles (user_id, full_name, avatar_url, email)
+  values (new.id, new.raw_user_meta_data->>'full_name', new.raw_user_meta_data->>'avatar_url', new.email);
+  return new;
+end;$function$
+;
+
+CREATE TRIGGER on_new_user_created AFTER INSERT ON auth.users FOR EACH ROW EXECUTE FUNCTION auth.create_new_profile();
