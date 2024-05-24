9840e35231ac65204bbbfd5d5aa3f8af06fdda61
Vitaliy Hula <gvnhak@gmail.com>
2024-05-24 22:00:05 +0300

    Squash db schema

--- frontend/supabase/migrations/20240524182435_disable_add_neww_user_trigger.sql
+++
@@ -1,4 +0,0 @@
-drop trigger if exists "on_new_user_created" on "auth"."users";
-
-CREATE TRIGGER on_new_user_created AFTER INSERT ON auth.users FOR EACH ROW EXECUTE FUNCTION auth.create_new_profile();
-ALTER TABLE "auth"."users" DISABLE TRIGGER "on_new_user_created";
