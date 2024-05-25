a0dc73218070a26f9a16d96ed27670d2d8a394b2
yuramol <yuramol86@gmail.com>
2024-05-25 09:32:02 +0300

    Merge branch 'dev' of https://github.com/Moonshot-Era/moonshot into feat/withdraw

--- frontend/src/app/page.tsx
+++ frontend/src/app/page.tsx
@@ -7,8 +7,9 @@ import { HomeContent } from '@/components/HomeContent/HomeContent';
 import { Header } from '@/components/Header/Header';
 import { WalletPortfolioNormilizedType } from '@/services/birdeye/getWalletPortfolio';
 import { ROUTES } from '@/utils';
+import { cookies } from 'next/headers';
 
-export default async function Index() {
+export default async function Home() {
   const supabaseClient = createServerClient();
 
   const user = (await supabaseClient.auth.getSession()).data.session?.user;
@@ -16,6 +17,14 @@ export default async function Index() {
   if (!user) {
     redirect(ROUTES.login);
   }
+
+  const oidc = cookies()?.get('pt')?.value;
+
+  const { data: walletData } = await axios.post(
+    `${process.env.NEXT_PUBLIC_SITE_URL}/api/cube/get-wallet`,
+    {
+      oidc,
+    });
 
   const { data } = await axios.post(
     `${process.env.NEXT_PUBLIC_SITE_URL}/api/birdeye/wallet-portfolio`,
