93c6dadd68b34ab337d934e5c4c0e360325700b4
yuramol <yuramol86@gmail.com>
2024-05-30 21:32:40 +0300

    Fix api url

--- frontend/src/services/jupiter/getSwapRoutes.ts
+++ frontend/src/services/jupiter/getSwapRoutes.ts
@@ -7,17 +7,22 @@ export const getSwapRoutes = async (
   slippageBps: string,
 ) => {
   try {
-    const quoteResponse = await axios.get(`${process.env.JUPITER_URL}/v6/quote`, {
-      params: {
-        inputMint: inputMint === 'So11111111111111111111111111111111111111111'
-          ? 'So11111111111111111111111111111111111111112' : inputMint,
-        outputMint,
-        amount,
-        wrapAndUnwrapSol: true,
-        asLegacyTransaction: true,
-        autoSlippage: true,
-      },
-    });
+    const quoteResponse = await axios.get(
+      `${process.env.JUPITER_URL}v6/quote`,
+      {
+        params: {
+          inputMint:
+            inputMint === 'So11111111111111111111111111111111111111111'
+              ? 'So11111111111111111111111111111111111111112'
+              : inputMint,
+          outputMint,
+          amount,
+          wrapAndUnwrapSol: true,
+          asLegacyTransaction: true,
+          autoSlippage: true
+        }
+      }
+    );
 
     return quoteResponse;
   } catch (err) {
