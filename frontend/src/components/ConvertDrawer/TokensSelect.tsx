7b70b0286d760cbcef5fc9780eef8c71e7a091c6
yuramol <yuramol86@gmail.com>
2024-05-30 13:28:43 +0300

    Ad assets search

--- frontend/src/components/ConvertDrawer/TokensSelect.tsx
+++ frontend/src/components/ConvertDrawer/TokensSelect.tsx
@@ -1,6 +1,6 @@
 'use client';
 
-import { FC } from 'react';
+import { ChangeEvent, FC, useCallback, useEffect, useState } from 'react';
 import { Flex, Text } from '@radix-ui/themes';
 import { AssetCard, Input, TokenCard } from '@/legos';
 
@@ -8,6 +8,9 @@ import './style.scss';
 import { PoolGeckoType } from '@/@types/gecko';
 import { WalletPortfolioAssetType } from '@/services/birdeye/getWalletPortfolio';
 import { TokenItemBirdEyeType } from '@/@types/birdeye';
+import { useSearchPools } from '@/hooks/useSearchPools';
+import debounce from 'lodash.debounce';
+import { set } from 'lodash';
 
 interface Props {
   tokensList: WalletPortfolioAssetType[] | PoolGeckoType[];
@@ -19,43 +22,113 @@ export const TokensSelect: FC<Props> = ({
   tokensList,
   handleTokenSelect,
   selectMode
-}) => (
-  <Flex
-    width="100%"
-    direction="column"
-    align="center"
-    gap="4"
-    pb="6"
-    position="relative"
-  >
-    <Flex className="search-input-holder" pb="2" px="4" direction="column">
-      <Text size="4" weight="bold" align="center" mb="2">
-        Convert {selectMode === 'to' ? 'to' : 'from'}
-      </Text>
-      <Input placeholder="Search assets" icon="search" />
-    </Flex>
-    <Flex width="100%" direction="column" gap="4" px="4">
-      {selectMode === 'from'
-        ? (tokensList as WalletPortfolioAssetType[])?.map(
-            (token: WalletPortfolioAssetType) => {
-              return (
-                <AssetCard
-                  key={token?.address}
-                  asset={token}
-                  onClick={() => handleTokenSelect(token)}
-                />
-              );
-            }
+}) => {
+  const [search, setSearch] = useState('');
+  const [filteredPools, setFilteredPools] = useState<
+    WalletPortfolioAssetType[]
+  >([]);
+
+  const { searchPools, refetch, isFetching } = useSearchPools(search);
+
+  const debouncedSearchPools = useCallback(
+    debounce(async (searchQuery) => {
+      if (selectMode === 'to') {
+        await refetch(searchQuery);
+      } else {
+        setFilteredPools(
+          (tokensList as WalletPortfolioAssetType[])?.filter(
+            (item: WalletPortfolioAssetType) =>
+              item?.name
+                ?.trim()
+                ?.toLowerCase()
+                ?.includes(searchQuery?.trim()?.toLowerCase()) ||
+              item?.address
+                ?.trim()
+                ?.toLowerCase()
+                ?.includes(searchQuery?.trim()?.toLowerCase())
           )
-        : (tokensList as PoolGeckoType[])?.map((token: PoolGeckoType) => {
-            return (
-              <TokenCard
-                key={token?.id}
-                token={token}
-                onClick={() => handleTokenSelect(token)}
-              />
-            );
-          })}
+        );
+      }
+    }, 300),
+    []
+  );
+
+  useEffect(() => {
+    if (search) {
+      debouncedSearchPools(search);
+    }
+  }, [debouncedSearchPools, search]);
+
+  const handleSearchChange = (event: ChangeEvent<HTMLInputElement>) => {
+    setSearch(event.target.value);
+  };
+
+  return (
+    <Flex
+      width="100%"
+      direction="column"
+      align="center"
+      gap="4"
+      pb="6"
+      position="relative"
+    >
+      <Flex className="search-input-holder" pb="2" px="4" direction="column">
+        <Text size="4" weight="bold" align="center" mb="2">
+          Convert {selectMode === 'to' ? 'to' : 'from'}
+        </Text>
+        <Input
+          placeholder="Search assets"
+          icon="search"
+          onChange={handleSearchChange}
+          value={search}
+        />
+      </Flex>
+      <Flex width="100%" direction="column" gap="4" px="4">
+        {selectMode === 'from'
+          ? search && searchPools?.length
+            ? filteredPools?.map((token) => {
+                return (
+                  <AssetCard
+                    key={token?.address}
+                    asset={token}
+                    onClick={() => handleTokenSelect(token)}
+                  />
+                );
+              })
+            : (tokensList as WalletPortfolioAssetType[])?.map(
+                (token: WalletPortfolioAssetType) => {
+                  return (
+                    <AssetCard
+                      key={token?.address}
+                      asset={token}
+                      onClick={() => handleTokenSelect(token)}
+                    />
+                  );
+                }
+              )
+          : null}
+        {selectMode === 'to'
+          ? search && searchPools?.length
+            ? (searchPools as PoolGeckoType[])?.map((token: PoolGeckoType) => {
+                return (
+                  <TokenCard
+                    key={`search-${token?.id}`}
+                    token={token}
+                    onClick={() => handleTokenSelect(token)}
+                  />
+                );
+              })
+            : (tokensList as PoolGeckoType[])?.map((token: PoolGeckoType) => {
+                return (
+                  <TokenCard
+                    key={token?.id}
+                    token={token}
+                    onClick={() => handleTokenSelect(token)}
+                  />
+                );
+              })
+          : null}
+      </Flex>
     </Flex>
-  </Flex>
-);
+  );
+};
