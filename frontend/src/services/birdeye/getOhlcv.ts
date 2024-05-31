1a44c9c1457e35ce5f43ade7cfb677e182f3b89d
yuramol <yuramol86@gmail.com>
2024-05-31 11:55:41 +0300

    Add param obj

--- frontend/src/services/birdeye/getOhlcv.ts
+++ frontend/src/services/birdeye/getOhlcv.ts
@@ -7,8 +7,12 @@ export const getOhlcvData = async (tokenAddress: string) => {
     }
 
     const { data } = await axios.get(
-      `${process.env.BIRDEYE_URL_API}defi/ohlcv?address=So${tokenAddress}&type=15m`,
+      `${process.env.BIRDEYE_URL_API}defi/ohlcv`,
       {
+        params: {
+          address: tokenAddress,
+          type: '15m'
+        },
         headers: {
           'x-chain': 'solana',
           'X-API-KEY': process.env.BIRDEYE_X_API_KEY
