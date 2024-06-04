65cf3f6b327ec7f9f33c55281e67e4a4d4973032
Anastasia <anastasia.pavlova.m@gmail.com>
2024-06-04 14:57:41 +0200

    chore: moved api out from gecko folder

--- frontend/src/app/culture/[address]/page.tsx
+++ frontend/src/app/culture/[address]/page.tsx
@@ -1,9 +1,9 @@
 import axios from 'axios';
 import { cookies } from 'next/headers';
 
+import { CultureError } from '@/components/CultureError/CultureError';
 import { CultureItem } from '@/components/CultureItem/CultureItem';
 import { checkProtectedRoute } from '@/utils/checkProtectedRoute';
-import { CultureError } from '@/components/CultureError/CultureError';
 
 export default async function CultureItemPage({
   params,
@@ -15,7 +15,7 @@ export default async function CultureItemPage({
   const tokenAddress = params?.address;
 
   const { data: token } = await axios.post(
-    `${process.env.NEXT_PUBLIC_SITE_URL}/api/gecko/get-token-data`,
+    `${process.env.NEXT_PUBLIC_SITE_URL}/api/get-token-data`,
     {
       tokenAddress
     }
