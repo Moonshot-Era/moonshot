36d6730e1907bd38b6ca4e7aed51717b77bdca60
Andrii Havryliuk <andrii.havryliuk@scoutgaminggroup.com>
2024-06-04 20:51:57 +0300

    logout on cubies forbidden error

--- frontend/src/app/page.tsx
+++ frontend/src/app/page.tsx
@@ -1,12 +1,11 @@
-import axios from "axios";
-import { cookies } from "next/headers";
-import { redirect } from "next/navigation";
+import axios from 'axios';
+import { cookies } from 'next/headers';
+import { redirect } from 'next/navigation';
 
 import { HomeContent } from '@/components/HomeContent/HomeContent';
 import { Header } from '@/components/Header/Header';
 import { checkProtectedRoute } from '@/utils/checkProtectedRoute';
 import { createBrowserClient } from '@/supabase/client';
-import { logout } from '@/utils';
 
 export default async function Home({ searchParams }: ServerPageProps) {
   await checkProtectedRoute(searchParams);
@@ -14,19 +13,17 @@ export default async function Home({ searchParams }: ServerPageProps) {
   const { data: sessionData } = await supabaseClient.auth.getSession();
 
   const userId = sessionData.session?.user?.id;
-  const oidc = cookies()?.get("pt")?.value;
+  const oidc = cookies()?.get('pt')?.value;
 
   const { data: walletData } = await axios.post(
     `${process.env.NEXT_PUBLIC_SITE_URL}/api/cube/get-wallet`,
     {
-      oidc,
+      oidc
     }
   );
 
   if (!walletData?.wallet) {
-    logout().then(() => {
-      redirect("/login");
-    });
+    redirect('/logout');
   }
 
   return (
