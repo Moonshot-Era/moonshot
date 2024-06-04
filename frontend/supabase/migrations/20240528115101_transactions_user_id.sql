69cc0c5b0882fcf6970b19930d3735da41ee366f
Vitaliy Hula <gvnhak@gmail.com>
2024-06-04 18:13:22 +0300

    fix: repair migration

--- frontend/supabase/migrations/20240528115101_transactions_user_id.sql
+++
@@ -1,5 +0,0 @@
-alter table "public"."transactions" drop constraint "transactions_user_id_fkey";
-
-alter table "public"."transactions" alter column "user_id" set data type uuid using "user_id"::uuid;
-
-
