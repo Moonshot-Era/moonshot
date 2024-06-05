6af8e1388122e780107d108a9441b321738ba124
Vitaliy Hula <gvnhak@gmail.com>
2024-06-05 08:04:45 +0300

    feat: Update store/get_refresh_token function

---
+++ frontend/supabase/migrations/20240605050305_update_store_get_refresh_function.sql
@@ -0,0 +1,52 @@
+CREATE OR REPLACE FUNCTION public.get_refresh_token(user_id uuid)
+ RETURNS text
+ LANGUAGE plpgsql
+ SECURITY DEFINER
+AS $function$
+DECLARE
+  encrypted_token TEXT;
+  decrypted_token TEXT;
+  encryption_key TEXT;
+BEGIN
+  SELECT decrypted_secret INTO encryption_key
+  FROM vault.decrypted_secrets
+  WHERE key_id = '94b538de-47d6-4116-9f6a-9b1cfbdce5c0';
+
+  IF encryption_key IS NULL THEN
+    RETURN NULL;
+  END IF;
+
+  SELECT rt INTO encrypted_token FROM profiles WHERE profiles.user_id = user_id;
+
+
+  IF encrypted_token IS NULL THEN
+    RETURN NULL;
+  END IF;
+
+  decrypted_token := pgp_sym_decrypt(encrypted_token, encryption_key);
+
+  RETURN decrypted_token;
+END;
+$function$
+;
+
+CREATE OR REPLACE FUNCTION public.store_refresh_token(user_id uuid, refresh_token text)
+ RETURNS void
+ LANGUAGE plpgsql
+ SECURITY DEFINER
+AS $function$
+DECLARE
+  encrypted_token TEXT;
+  encryption_key TEXT;
+BEGIN
+  SELECT decrypted_secret INTO encryption_key
+  FROM vault.decrypted_secrets
+  WHERE key_id = '94b538de-47d6-4116-9f6a-9b1cfbdce5c0';
+
+  encrypted_token := pgp_sym_encrypt(refresh_token, encryption_key);
+
+  UPDATE profiles SET rt = encrypted_token
+  WHERE profiles.user_id = user_id;
+END;
+$function$
+;
