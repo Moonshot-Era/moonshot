ff840727001d52025e38c31652c09b2dfd4f5d33
yuramol <yuramol86@gmail.com>
2024-06-05 13:55:36 +0300

    Add shift4 sandbox

--- frontend/src/app/layout.tsx
+++ frontend/src/app/layout.tsx
@@ -78,9 +78,7 @@ export default function RootLayout({
         <Script
           src={`https://progressier.app/${process.env.NEXT_PUBLIC_PROGRESSIER_ID}/script.js`}
         />
-        <Script
-          src={`https://crypto.shift4.com/sdk/v1/shift4crypto-sdk-latest.js`}
-        />
+        <Script src={`${process.env.SHIFT4_URL}/shift4crypto-sdk-latest.js`} />
         <Toaster />
         <ServiceWorkerRegister />
         <SplashScreen />
