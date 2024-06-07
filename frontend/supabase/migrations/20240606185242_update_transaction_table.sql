9b583a39f58fda70492cdc3d5b16cda000cdc567
Anastasia <anastasia.pavlova.m@gmail.com>
2024-06-07 11:17:45 +0200

    fix: migrations, schema

--- frontend/supabase/migrations/20240606185242_update_transaction_table.sql
+++ frontend/supabase/migrations/20240606185242_update_transaction_table.sql
@@ -4,10 +4,6 @@ alter table "public"."transactions" drop column "date_purchased";
 
 alter table "public"."transactions" drop column "date_sold";
 
-alter table "public"."transactions" add column "image_url" text;
-
 alter table "public"."transactions" add column "token_address" text;
 
 alter table "public"."transactions" add column "transaction_type" transaction_type;
-
-alter table "public"."transactions" add column "wallet_address" text;
