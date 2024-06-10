964473deb1efd57daf2de685beb996e237d397bf
yuramol <yuramol86@gmail.com>
2024-06-10 12:11:25 +0300

    Add 1.3 case

--- frontend/src/helpers/calculateShareImageData.ts
+++ frontend/src/helpers/calculateShareImageData.ts
@@ -4,46 +4,73 @@ export const calculateShareImageData = (
   transactions: Tables<'transactions'>[],
   tokenPrice: number
 ) => {
-  const purchaseTransactions = transactions.filter(
-    (item) => item.transaction_type === 'buy'
+  let totalRevenueAmount = 0;
+  let totalRevenuePercent = 0;
+
+  const totalRevenueAll = transactions.reduce(
+    (acc, cur) => {
+      if (cur.transaction_type === 'buy') {
+        acc.buyVolume += cur.token_amount ?? 0;
+        acc.buyAmountTotal += (cur.token_amount ?? 0) * (cur.token_price ?? 0);
+      }
+
+      if (cur.transaction_type === 'sell') {
+        acc.sellVolume += cur.token_amount ?? 0;
+        acc.sellAmountTotal += (cur.token_amount ?? 0) * (cur.token_price ?? 0);
+      }
+
+      return acc;
+    },
+    { buyVolume: 0, sellVolume: 0, buyAmountTotal: 0, sellAmountTotal: 0 }
   );
-  const sellTransactions = transactions.filter(
-    (item) => item.transaction_type === 'sell'
-  );
 
-  const lastPurchaseTransaction =
-    purchaseTransactions[purchaseTransactions.length - 1];
-  const lastSellTransaction = sellTransactions[sellTransactions.length - 1];
-
-  const totalCost = purchaseTransactions.reduce((acc, transaction) => {
-    return (
-      acc + (transaction.token_amount || 0) * (transaction.token_price || 0)
-    );
-  }, 0);
-
-  let totalRevenue;
-
-  if (!sellTransactions.length) {
-    totalRevenue = purchaseTransactions.reduce((acc, transaction) => {
-      console.log(
-        'transaction.token_amount',
-        transaction.token_amount,
-        '*',
-        tokenPrice
-      );
-      return acc + (transaction.token_amount || 0) * tokenPrice;
-    }, 0);
-  } else {
-    totalRevenue = sellTransactions.reduce((acc, transaction) => {
-      return (
-        acc + (transaction.token_amount || 0) * (transaction.token_price || 0)
-      );
-    }, 0);
+  if (totalRevenueAll.buyVolume > totalRevenueAll.sellVolume) {
+    totalRevenueAmount =
+      totalRevenueAll.sellAmountTotal +
+      (totalRevenueAll.buyVolume - totalRevenueAll.sellVolume) * tokenPrice -
+      totalRevenueAll.buyAmountTotal;
+    totalRevenuePercent =
+      (totalRevenueAmount / totalRevenueAll.buyAmountTotal) * 100;
   }
 
-  const profit = totalRevenue - totalCost;
+  // const purchaseTransactions = transactions.filter(
+  //   (item) => item.transaction_type === 'buy'
+  // );
+  // const sellTransactions = transactions.filter(
+  //   (item) => item.transaction_type === 'sell'
+  // );
 
-  const profitPercent = (profit / totalCost) * 100;
+  // const lastPurchaseTransaction =
+  //   purchaseTransactions[purchaseTransactions.length - 1];
+  // const lastSellTransaction = sellTransactions[sellTransactions.length - 1];
+
+  // const totalCost = purchaseTransactions.reduce((acc, transaction) => {
+  //   return (
+  //     acc + (transaction.token_amount || 0) * (transaction.token_price || 0)
+  //   );
+  // }, 0);
+
+  // if (!sellTransactions.length) {
+  //   totalRevenue = purchaseTransactions.reduce((acc, transaction) => {
+  //     console.log(
+  //       'transaction.token_amount',
+  //       transaction.token_amount,
+  //       '*',
+  //       tokenPrice
+  //     );
+  //     return acc + (transaction.token_amount || 0) * tokenPrice;
+  //   }, 0);
+  // } else {
+  //   totalRevenue = sellTransactions.reduce((acc, transaction) => {
+  //     return (
+  //       acc + (transaction.token_amount || 0) * (transaction.token_price || 0)
+  //     );
+  //   }, 0);
+  // }
+
+  const profit = totalRevenueAmount;
+
+  const profitPercent = totalRevenuePercent;
 
   const datePurchased = new Date(
     lastPurchaseTransaction.created_at || ''
