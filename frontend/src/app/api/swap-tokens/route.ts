ef4210bbea7e9602c580c288005d02e27cec0a52
Yurij Moldavchyk <yuramoldev@gmail.com>
2024-06-14 17:53:46 +0300

    Feat/platform fee (#86)
    * Create convert fee instructions
    
    * Add symbol to export, fix SOL percentage

--- frontend/src/app/api/swap-tokens/route.ts
+++ frontend/src/app/api/swap-tokens/route.ts
@@ -1,64 +1,22 @@
 import { cookies } from 'next/headers';
 import { NextResponse } from 'next/server';
 import { swapTokens } from '@/services/jupiter/swapTokens';
-import { sendTokensTransaction } from '@/services/solana/sendTokensTransaction';
-import {
-  MOONSHOT_FEE,
-  MOONSHOT_WALLET_ADDRESS,
-  REF_FEE,
-  REF_WALLET_ADDRESS
-} from '@/utils';
-import { isSolanaAddress } from '@/helpers/helpers';
-import { sendNativeTransaction } from '@/services/solana/sendNativeTransaction';
 
 export async function POST(request: Request) {
   const data = await request.json();
   const oidc = cookies()?.get('pt')?.value;
 
   if (!oidc) {
-    return NextResponse.next({
-      request: {
-        headers: request.headers
-      }
-    });
+    return NextResponse.json({ error: { statusText: 'Forbidden' } });
   }
 
-  const txid = await swapTokens(oidc!, data.swapRoutes, data.feeData);
-  // if (txid) {
-  //   if (isSolanaAddress(data.feeData.tokenAddress)) {
-  //     await sendNativeTransaction(
-  //       oidc!,
-  //       data.feeData.fromAddress,
-  //       MOONSHOT_WALLET_ADDRESS,
-  //       +data.feeData.amount * MOONSHOT_FEE
-  //     );
-  //     await sendNativeTransaction(
-  //       oidc!,
-  //       data.feeData.fromAddress,
-  //       REF_WALLET_ADDRESS,
-  //       +data.feeData.amount * REF_FEE
-  //     );
-  //   } else {
-  //     await sendTokensTransaction(
-  //       oidc!,
-  //       data.feeData.fromAddress,
-  //       MOONSHOT_WALLET_ADDRESS,
-  //       +data.feeData.amount * MOONSHOT_FEE,
-  //       data.feeData.tokenAddress,
-  //       data.feeData.tokenDecimals
-  //     );
-  //     await sendTokensTransaction(
-  //       oidc!,
-  //       data.feeData.fromAddress,
-  //       REF_WALLET_ADDRESS,
-  //       +data.feeData.amount * REF_FEE,
-  //       data.feeData.tokenAddress,
-  //       data.feeData.tokenDecimals
-  //     );
-  //   }
-  // }
+  try {
+    const txid = await swapTokens(oidc!, data.swapRoutes, data.feeData);
 
-  return NextResponse.json({
-    txid
-  });
+    return NextResponse.json({
+      txid
+    });
+  } catch (err) {
+    return NextResponse.json({ error: err });
+  }
 }
---
+++ frontend/src/services/solana/createTokensTxInstruction.ts
@@ -0,0 +1,92 @@
+import {
+  Connection,
+  LAMPORTS_PER_SOL,
+  PublicKey,
+  Transaction
+} from '@solana/web3.js';
+
+import {
+  createTransferCheckedInstruction,
+  TOKEN_PROGRAM_ID,
+  ASSOCIATED_TOKEN_PROGRAM_ID,
+  getAssociatedTokenAddress,
+  createAssociatedTokenAccountInstruction,
+  getAccount
+} from '@solana/spl-token';
+
+interface Props {
+  fromPublicKey: PublicKey;
+  toAddress: string;
+  tokenAddress: string;
+  amount: number;
+  tokenDecimals: number;
+  tx: Transaction;
+  connection: Connection;
+}
+
+export const createTokensTxInstruction = async ({
+  fromPublicKey,
+  toAddress,
+  tokenAddress,
+  amount,
+  tokenDecimals,
+  tx,
+  connection
+}: Props) => {
+  const toPubkey = new PublicKey(toAddress);
+
+  const mintPubkey = new PublicKey(tokenAddress);
+
+  console.log(`Transferring ${amount} from ${fromPublicKey} to ${toPubkey}`);
+
+  // Get the associated token accounts for the sender and receiver
+  const fromTokenAccount = await getAssociatedTokenAddress(
+    mintPubkey, // mint
+    fromPublicKey, // from owner
+    true, // allow owner off curve
+    TOKEN_PROGRAM_ID,
+    ASSOCIATED_TOKEN_PROGRAM_ID
+  );
+
+  const toTokenAccount = await getAssociatedTokenAddress(
+    mintPubkey, // mint
+    toPubkey, // to owner
+    true, // allow owner off curve
+    TOKEN_PROGRAM_ID,
+    ASSOCIATED_TOKEN_PROGRAM_ID
+  );
+
+  let tokenAccount;
+  try {
+    const res = await getAccount(connection, toTokenAccount);
+    if (res) {
+      tokenAccount = res;
+    }
+  } catch (err) {
+    console.log(`Token account error: ${err}`);
+  }
+
+  if (!tokenAccount) {
+    tx.add(
+      createAssociatedTokenAccountInstruction(
+        fromPublicKey,
+        toTokenAccount,
+        toPubkey,
+        mintPubkey,
+        TOKEN_PROGRAM_ID,
+        ASSOCIATED_TOKEN_PROGRAM_ID
+      )
+    );
+  }
+
+  tx.add(
+    createTransferCheckedInstruction(
+      fromTokenAccount, // from
+      mintPubkey, // mint
+      toTokenAccount, // to
+      fromPublicKey, // from's owner
+      amount * (10 ** +tokenDecimals / LAMPORTS_PER_SOL) * LAMPORTS_PER_SOL, // amount
+      tokenDecimals // decimals
+    )
+  );
+};
