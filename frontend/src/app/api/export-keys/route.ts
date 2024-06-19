3275c91067690b105bfb45d70a8bb404a86de596
yuramol <yuramol86@gmail.com>
2024-06-19 12:46:28 +0300

    Remove unused import

--- frontend/src/app/api/export-keys/route.ts
+++ frontend/src/app/api/export-keys/route.ts
@@ -1,7 +1,6 @@
 import { NextResponse } from 'next/server';
 import { fetchExportKeys, fetchInitiateExportKeys } from '@/services';
 import { cookies } from 'next/headers';
-import { getMfaSecret } from '@/services/helpers/getMfaSecret';
 
 export async function POST(request: Request) {
   const oidc = cookies()?.get('pt')?.value;
