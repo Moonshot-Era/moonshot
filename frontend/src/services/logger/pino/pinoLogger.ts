c919de171690c217549ea9f36961468b733ac9f3
Vitaliy Hula <gvnhak@gmail.com>
2024-06-19 15:18:25 +0300

    implement logger

---
+++ frontend/src/services/logger/pino/pinoLogger.ts
@@ -0,0 +1,48 @@
+// logger.js
+import pino from 'pino';
+import fs from 'fs';
+import { createStream } from 'rotating-file-stream';
+
+const logDir = `${process.env.PATH_TO_DEPLOY}/logs`;
+
+// Ensure logs directory exists
+if (!fs.existsSync(logDir)) {
+  fs.mkdirSync(logDir);
+}
+
+const isProduction = process.env.NODE_ENV === 'production';
+
+const pinoLogger = pino(
+  {
+    formatters: {
+      level: (label) => {
+        return { level: label };
+      }
+    },
+    level: 'info',
+    timestamp: pino.stdTimeFunctions.isoTime
+  },
+
+  createStream(
+    () => {
+      const date = new Date().toISOString();
+
+      return `moonshot_${date.substring(0, 10)}.log`;
+    },
+    {
+      interval: '1d', // rotate daily
+      path: logDir
+    }
+  )
+);
+
+export const logger = {
+  info: (...msg: any) =>
+    !isProduction
+      ? pinoLogger.info(msg)
+      : console.log('\x1b[34m%s\x1b[0m', 'Info: ', ...msg),
+  error: (...msg: any) =>
+    !isProduction
+      ? pinoLogger.error(msg)
+      : console.log('\x1b[31m%s\x1b[0m', 'Error: ', ...msg)
+};
