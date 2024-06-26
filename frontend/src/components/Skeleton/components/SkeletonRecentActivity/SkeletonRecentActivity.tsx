0225d38a3c2e7cee7d681767e87769b835e3144b
vitalekp <vitalekpushkarov@gmail.com>
2024-06-26 12:45:11 +0300

    Show loading skeleton for recent activity

---
+++ frontend/src/components/Skeleton/components/SkeletonRecentActivity/SkeletonRecentActivity.tsx
@@ -0,0 +1,63 @@
+import { Box, Flex } from '@radix-ui/themes';
+import Skeleton from 'react-loading-skeleton';
+import 'react-loading-skeleton/dist/skeleton.css';
+
+import { Icon } from '@/legos';
+import './style.scss';
+
+export const SkeletonRecentActivity = () => (
+  <Flex
+    width="100%"
+    direction="column"
+    gap="4"
+    overflow="hidden"
+    pr="1"
+    pb="2px"
+  >
+    {Array(3)
+      .fill(3)
+      .map((_, index) => (
+        <Flex
+          key={index}
+          direction="row"
+          align="center"
+          justify="between"
+          gap="2"
+          width="100%"
+          overflow="hidden"
+          className={`border-1 bg-white skeleton-empty-card`}
+          p="3"
+          pt="2"
+        >
+          <Flex direction="row" gap="2">
+            <Box>
+              <Skeleton
+                width={48}
+                height={48}
+                className="skeleton-empty-card-icon"
+              />
+            </Box>
+            <Flex direction="column">
+              <Box>
+                <Skeleton width={67} />
+              </Box>
+              <Box>
+                <Skeleton width={42} height={8} />
+              </Box>
+            </Flex>
+          </Flex>
+          <Flex direction="row" align="center" gap="2">
+            <Flex direction="column" align="end">
+              <Box>
+                <Skeleton width={67} />
+              </Box>
+              <Box>
+                <Skeleton width={42} height={8} />
+              </Box>
+            </Flex>
+            <Icon icon="chevronRight" />
+          </Flex>
+        </Flex>
+      ))}
+  </Flex>
+);
